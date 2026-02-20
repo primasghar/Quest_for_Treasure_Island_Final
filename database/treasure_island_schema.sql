aedcfda2278b64ef3d2e13f34bfe10ae27aee284
Prim Asghar <mirpasghar@gmail.com>
2026-02-20 23:19:50 +0200

    Edited schema

--- database/treasure_island_schema.sql
+++ database/treasure_island_schema.sql
@@ -1,8 +1,8 @@
 
 create table game_airports(
       id int not null auto_increment,
-      airport_id int,
-	  primary key (id)
+      airport_id int ,
+	  primary key (id),
       foreign key (airport_id) references airport(ident)
   )
 
@@ -15,26 +15,27 @@ create table player(
 create table progress(
       id int not null auto_increment,
       player_id int,
-      current_stage int null,
+      current_stage int,
       game_score int,
-      carbon_footprint int null,
-	  primary key (id)
+      carbon_footprint int,
+	  primary key (id),
       foreign key (player_id) references player(id)
   )
 
 create table games(
       id int not null auto_increment,
-      game_name varchar(40) null,
+      game_name varchar(40),
       game_airports_id int,
       difficulty_level int,
-	  primary key (id)
+	  primary key (id),
       foreign key (game_airports_id) references game_airports(id)
   )
 
 create table quizlet(
       id int not null auto_increment,
       question varchar(255)
-      correct_answer varchar(40) null,
+      correct_answer varchar(40),
+      answer_choices varchar(255),
       difficulty_level int,
 	  primary key (id)
   )
