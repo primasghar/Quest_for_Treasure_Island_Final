c9d561b334daf8fd4cc7acdbb8d4703a061055c7
Prim Asghar <mirpasghar@gmail.com>
2026-02-21 22:15:34 +0200

    Added sql query to insert game_airports

---
+++ database/queries.sql
@@ -0,0 +1,15 @@
+
+--Query for adding 10 airports for game using the ICAO code (ident - country table)
+insert into game_airports (airport_id)
+values ("EFHK", "EETN", "EPWA","LROP", "LDZA", " LIMC", "LPPT", "LFPG", "EIDW", "KJFK");
+
+-- EFHK - Helsinki Vantaa Airport (Helsinki, Finland)
+-- EETN - Lennart Meri Tallinn Airport (Tallinn, Estonia)
+-- EPWA - Warsaw Chopin Airport (Warsaw, Poland)
+-- LROP - Henri Coand? International Airport (Bucharest, Romania)
+-- LDZA - Zagreb Airport (Zagreb or Dubrovnik, Croatia)
+-- LIMC - Malpensa International Airport (Milan, Italy)
+-- LPPT - Humberto Delgado Airport (Lisbon Portela (Lisbon, Portugal)
+-- LFPG - Charles de Gaulle International Airport (Paris, France)
+-- EIDW - Dublin Airport (Dublin, Ireland)
+-- KJFK - John F Kennedy International Airport (New York, USA)
