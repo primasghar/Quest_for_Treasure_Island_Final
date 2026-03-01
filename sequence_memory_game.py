63b19beff6f6baeb78faa030e1ebfbc8468f57cc
Prim Asghar <mirpasghar@gmail.com>
2026-03-02 01:13:27 +0200

    Added the function for sequence memory game.

--- sequence_memory_game.py
+++ sequence_memory_game.py
@@ -1,9 +1,34 @@
 import string
 import random
+import sys, time
 
-# All letters and numbers from which the random combination will be generated.
-letters_and_numbers = string.ascii_uppercase + string.digits
-# print(letters_and_numbers)
+def sequence_memory_game():
 
-sequence_size = 10
-print("".join(random.choices(letters_and_numbers, k = sequence_size)))
+    letters_and_numbers = string.ascii_uppercase
+    # print(letters_and_numbers)
+
+    sequence_size = 8
+    delimiter = ""
+    random_sequence = delimiter.join((random.choices(letters_and_numbers, k=sequence_size)))
+
+    print(random_sequence, end='')
+    time.sleep(8)
+    print(8 * '\b' + "Please enter the sequence")
+
+    trial = 0
+    user_entered_sequence = ""
+
+    while user_entered_sequence != random_sequence and trial < 3:
+        trial += 1
+        user_entered_sequence = input("Enter sequence: ")
+        # print(f"trial: {trial}")
+
+
+    if user_entered_sequence == random_sequence:
+        print("Congratulations! You did it")
+        print(f"Your sequence: {user_entered_sequence} matched {random_sequence}")
+    else:
+        print(f"Sorry! Your sequence: {user_entered_sequence} does not match {random_sequence}. You have lost")
+
+
+sequence_memory_game()
