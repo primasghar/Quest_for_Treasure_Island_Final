c36fdf71c7c2e138615c240bd1b7c3a61fa1750e
Prim Asghar <mirpasghar@gmail.com>
2026-03-02 18:20:31 +0200

    Added quizlet questions with difficulty level 1

--- database/queries.sql
+++ database/queries.sql
@@ -13,3 +13,28 @@ values ("EFHK", "EETN", "EPWA","LROP", "LDZA", " LIMC", "LPPT", "LFPG", "EIDW", 
 -- LFPG - Charles de Gaulle International Airport (Paris, France)
 -- EIDW - Dublin Airport (Dublin, Ireland)
 -- KJFK - John F Kennedy International Airport (New York, USA)
+
+insert into quizlet (question, correct_answer, difficulty_level)
+values
+("What phone company produced the 3310?", "Nokia", 1),
+("What is the capital of Ireland?", "Dublin", 1),
+("Who is the President of Finland?", "Alexander Stubb", 1),
+("Which planet is closest to the sun?", "Mercury", 1),
+("In what year did World War II end? ", "1945", 1);
+("Who was the first woman to win a Nobel Prize?", "Marie Curie", 1);
+("What is the main ingredient in hummus?", "Chickpeas", 1);
+("Where did sushi originate?", "Japan", 1);
+("What was the first name of Argentinian soccer star Maradona?", "Diego", 1);
+("Which country has won the most FIFA World Cups?", "Brazil", 1);
+("What is the national sport of Japan?", "Sumo wrestling", 1);
+("What is the only mammal capable of true flight", "Bat", 1);
+("What is the largest mammal on Earth", "Blue Whale", 1);
+("Which rapper is also known as Slim Shady?", "Eminem", 1);
+("Who wrote The Hobbit?", "J.R.R. Tolkien", 1);
+("Which fruit has seeds on the outside?", " Strawberry", 1);
+("How many legs does a spider have?", "Eight", 1);
+("Which planet is known as the Red Planet?", "Mars", 1);
+("What is the largest ocean on Earth?", "Pacific", 1);
+("What does CPU stand for", "Central Processing Unit", 1);
+("What does URL stand for?" "Uniform Resource Locator", 1);
+("What is the term for harmful software such as viruses or ransomware?" "Malware", 1);
