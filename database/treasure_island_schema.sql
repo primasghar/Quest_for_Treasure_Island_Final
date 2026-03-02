a33aa4414968ab7442717fc22d83efd5b6583fab
Prim Asghar <mirpasghar@gmail.com>
2026-03-03 00:36:11 +0200

    Edited Schema to add default values for the player initial progress and added "CREATE TABLE IF NOT EXISTS"

--- database/treasure_island_schema.sql
+++ database/treasure_island_schema.sql
@@ -5,31 +5,31 @@
       FOREIGN KEY (airport_id) REFERENCES airport(ident)
   );
 
- CREATE TABLE player(
+ CREATE TABLE IF NOT EXISTS player(
       id INT NOT NULL auto_increment,
       name varchar(40) NULL,
 	  PRIMARY KEY (id)
   )
 
- CREATE TABLE progress(
+  CREATE TABLE IF NOT EXISTS progress(
       id INT NOT NULL auto_increment,
       player_id INT,
-      current_level INT,
-      game_score INT,
-      carbon_footprint INT,
+      current_level INT default 1,
+      game_score INT default 0,
+      carbon_footprint INT default 0,
 	  PRIMARY KEY (id),
       FOREIGN KEY (player_id) REFERENCES player(id),
       FOREIGN KEY (current_level) REFERENCES game_airports(id)
   )
 
- CREATE TABLE games(
+ CREATE TABLE IF NOT EXISTS games(
       id INT NOT NULL auto_increment,
       game_name varchar(40),
       difficulty_level INT,
 	  PRIMARY KEY (id),
   )
 
- CREATE TABLE quizlet(
+ CREATE TABLE IF NOT EXISTS quizlet(
       id INT NOT NULL auto_increment,
       question text,
       correct_answer text,
