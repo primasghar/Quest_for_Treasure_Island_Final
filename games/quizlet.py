ca75b11cded0159b12fc4d960f07840d1b59570e
Prim Asghar <mirpasghar@gmail.com>
2026-03-05 21:50:58 +0200

    Started code to fetch quiz questions by difficulty level

---
+++ games/quizlet.py
@@ -0,0 +1,15 @@
+import query_functions
+import random
+
+# list to save already asked questions
+asked_them_already = []
+
+def quiz_question():
+    # fetching questions list with difficulty level 1
+    questions = query_functions.fetch_quiz_questions(1)
+    # fetching one random question from list
+    question = random.choice(questions)
+    # Check: if the questions has been already asked using id
+    for ques in asked_them_already:
+        if ques[0] == question[0]:
+            break
