326f0bc8c2f43a5ede1f2260e5a1f5c4fbbe0024
Prim Asghar <mirpasghar@gmail.com>
2026-03-06 14:11:11 +0200

    Refactored code: Created functions to follow the "DRY" principle

--- main.py
+++ main.py
@@ -3,22 +3,33 @@ from query_functions import (add_player, player_progress_id, fetch_player_progre
                              fetch_game_airport_icao, fetch_airport_info, fetch_airport_country, update_progress)
 
 
-
 def airport_data(level):
-
     icao_code = fetch_game_airport_icao(level)
-
     airport_details = fetch_airport_info(icao_code[0])
     # print(airport_details)
-
     airport_in_country = fetch_airport_country(airport_details[0])
     # print(airport_in_country)
-
     airport_destination = airport_details + airport_in_country
     # [iso_country, ident, name(airport), latitude_deg, longitude_deg, name(country)]
-
     return airport_destination
 
+def airport_and_country(level):
+    next_destination = airport_data(level)
+    name_of_airport_and_country = next_destination[2], next_destination[5]
+    return name_of_airport_and_country
+
+
+def update_board(game_level, game_points, carbon_fp, player_Id):
+    print(f"Your next airport destination is {airport_and_country(game_level)}")
+    update_progress(game_level, game_points, carbon_fp, player_Id)
+
+def display_board(player_name, current_level, game_score, carbon_footprint, current_airport ):
+    print(f"""
+    Adventurer: {player_name}
+    Level: {current_level}
+    Game points: {game_score}
+    Carbon footprints: {carbon_footprint}
+    Location: {current_airport}\n""")
 
 # ---------------------------------------------------------------------------------------------------------------------
 # GAME START
@@ -34,26 +45,17 @@ player_Id = add_player(player_name)
 if player_Id:
     player_progress_id(player_Id)
 
-#progress table (id, player_id, current_level, game_score, carbon_footprint)
 player_progress_data = fetch_player_progress(player_Id)
 
+# Global Variables for score board
 current_level = player_progress_data[2]
 game_score = player_progress_data[3]
 carbon_footprint = player_progress_data[4]
-
-airport_and_country = airport_data(current_level)
-
-current_airport = f"{airport_and_country[2]}, {airport_and_country[5]}"
-
+current_airport = airport_and_country(current_level)
 
 print(f"Welcome! To the Quest for a Treasure Island.")
-print(f"""
-Adventurer: {player_name}
-Level: {current_level}
-Game points: {game_score}
-Carbon footprints: {carbon_footprint}
-Location: {current_airport}\n""")
 
+display_board(player_name, current_level, game_score, carbon_footprint, current_airport)
 
 if current_level == 1:
     result = rock_paper_scissors.rock_paper_scissors_game(player_name)
@@ -63,33 +65,14 @@ if current_level == 1:
         current_level = 2
         game_score += 1000
         carbon_footprint += 1000
-
-        airport_data(current_level)
-        current_airport = {airport_data[2]}, {airport_data[5]}
-
-        update_progress(current_level,game_score,carbon_footprint, current_airport)
+        current_airport = airport_and_country(current_level)
 
         print(f"Your next airport destination is {current_airport}")
+        update_progress(current_level, game_score, carbon_footprint, player_Id)
 
 
-elif current_level == 2:
+if current_level == 2:
     result = sequence_memory.sequence_memory_game(player_name)
-
-    if result:
-        current_level = 3
-        game_score += 1000
-        carbon_footprint += 1000
-
-        airport_data(current_level)
-        current_airport = f"{airport_data[2]}, {airport_data[5]}"
-
-        update_progress(current_level, game_score, carbon_footprint, current_airport)
-
-        print(f"Your next airport destination is {current_airport}")
-
-
-
-
 
 
 #clear the player and its progress. if player quits or loose
