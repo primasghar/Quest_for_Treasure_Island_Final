8eee0b8b123a1efbe0983a57fe7f1d5e6f80e891
Prim Asghar <mirpasghar@gmail.com>
2026-03-08 00:29:37 +0200

    Enhanced the roll a dice game to get 12 on three rolls.

--- games/roll_the_dice.py
+++ games/roll_the_dice.py
@@ -3,23 +3,42 @@ import random
 def roll_the_dice_game(name):
     print(f"""Game name: ROLL THE DICE
            {name}! You will have three opportunities to win the game and go to your next airport destination.
-           Please press enter key to roll the dice and try your luck to get a 6.\n""")
+           In each attempt you will roll the dice three times by pressing "Enter" button. The sum of three rolls 
+           will be added. If the sum is equal to 12. You will win.  
+           So! what are you waiting for, let's keep it rolling.\n""")
+
     win = False
+    lucky_number = 12
+    attempt = 0
 
-    for i in range(3):
+    while win != True and attempt < 3:
+        attempt += 1
+        print(f"\nAttempt no: {attempt}\n")
+        roll_list = []
 
-        input(f"Press Enter: ")
-        print(f"Attempt no: {i+1}")
 
-        roll = random.randint(1, 6)
-        print(f"You rolled: {roll}\n")
+        for i in range(1,4):
+            input(f"Press Enter: ")
 
-        if roll == 6:
+            roll = random.randint(1, 10)
+
+            print(f"You rolled: {roll}\n")
+            roll_list.append(roll)
+
+        total_of_rolls = sum(roll_list)
+
+        if total_of_rolls == lucky_number:
+            print(f"You sum of your rolls is {total_of_rolls} which is equal to 12. You won!")
             win = True
+        else:
+            print(f"You sum of your rolls is {total_of_rolls} which is not equal to 12. You lost!")
+            print("Please try again!")
 
     if win:
         print("\nCongratulations! You won the game!")
+        return win
     else:
-        print("\nSorry, you lost the game!")
+        print("\nSorry, You lost the game!")
+        return win
 
 roll_the_dice_game("Prim")
