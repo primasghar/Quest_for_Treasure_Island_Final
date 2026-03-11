9b688ed3fce012ca79798b348e6d92cfdbddc3d5
Prim Asghar <mirpasghar@gmail.com>
2026-03-11 22:24:46 +0200

    Refactored sequence_memory.py

--- games/sequence_memory.py
+++ games/sequence_memory.py
@@ -2,6 +2,7 @@ import string
 import random
 import time
 from colorama import Fore, Style
+
 def play_game(name):
     print("Game name: " + Fore.MAGENTA + "SEQUENCE MEMORY GAME")
     print(Style.RESET_ALL)
@@ -12,6 +13,7 @@ def play_game(name):
     attempt = 0
     user_entered_sequence = ""
     random_sequence = ""
+    matched = False
 
     while (random_sequence == "" or user_entered_sequence != random_sequence) and attempt < 3:
         attempt += 1
@@ -21,23 +23,33 @@ def play_game(name):
         sequence_size = 8
         delimiter = ""
 
-        user_permission = input("Press enter to see the sequence. Display time is 8 seconds: ")
+        user_permission = input("Press enter to see the sequence: ")
         if user_permission == "":
 
             random_sequence = delimiter.join((random.choices(letters, k=sequence_size)))
             print(random_sequence)
             time.sleep(8)
-
             print("\033[A", end="\r") # Clear the code
             print(8 * '\b' + "Please enter the sequence")
 
         user_entered_sequence = input("Enter sequence: ")
 
-    if user_entered_sequence == random_sequence:
-        print("\nCongratulations! You did it")
-        print(f"Your sequence: {user_entered_sequence} matched {random_sequence}")
-        return True
+        if user_entered_sequence == random_sequence:
+            matched = True
+            print(f"\nIt perfectly matched {random_sequence}")
+            print("You got awesome memory!")
+        else:
+            matched = False
+            print(f"\nIt did not match {random_sequence}")
+            print("Please try again with a new sequence")
+
+
+    if matched:
+        print("\nCongratulations! You won the game!")
+        return matched
     else:
-        print(f"\nSorry! Your sequence: {user_entered_sequence} does not match {random_sequence}. You have lost")
-        return False
+        print("\nSorry, you lost the game!")
+        return matched
+
+play_game("Prim")
 
