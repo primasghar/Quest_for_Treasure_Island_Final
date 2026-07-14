7df686ecae9d2e9377750da50c767269810438a7
Prim Asghar <mirpasghar@gmail.com>
2026-07-15 02:14:45 +0300

    "Enter" key press added in form submit for player name.

--- Frontend/pages/playerName/index.js
+++ Frontend/pages/playerName/index.js
@@ -1,15 +1,19 @@
 const form = document.querySelector(".nameForm");
+const nameInput = document.querySelector('[name=userName]');
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
     fetchNewPlayerData();
 })
 
+nameInput.addEventListener("keypress", (e)=>{
+    if(e.key === "Enter") form.click();
+})
+
 const fetchNewPlayerData = async () => {
-    let nameInput = document.querySelector('[name=userName]').value;
 
     try {
-        const response = await fetch(`http://127.0.0.1:5000/player/${nameInput}`)
+        const response = await fetch(`http://127.0.0.1:5000/player/${nameInput.value}`)
         const newPlayerData = await response.json();
         console.log("new player", newPlayerData)
         localStorage.setItem("playerDetails", JSON.stringify(newPlayerData) );
