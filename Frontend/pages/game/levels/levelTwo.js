85db14be751e9c19f292a615272fc7079a1a0734
Prim Asghar <mirpasghar@gmail.com>
2026-08-01 21:13:50 +0300

    Refactorings in the second level

--- Frontend/pages/game/levels/levelTwo.js
+++ Frontend/pages/game/levels/levelTwo.js
@@ -13,6 +13,7 @@ import { warningMessageModal } from '../utils/modals.js'
 import {
     getPlayerProgressData
 } from '../utils/localStorageFunctions.js'
+import {Attempts} from "../utils/enums.js";
 
 let levelTwoContainer;
 let describeGame2Para;
@@ -22,54 +23,64 @@ let buttonEl;
 let playerChoice;
 let computerChoice;
 
-let playerWon = "No";
+const GameStatus = {
+    NOT_WON : "No",
+    WON : "Yes",
+    DRAW: "draw"
+}
+const Selection = {
+    ROCK: "ROCK",
+    PAPER: "PAPER",
+    SCISSORS: "SCISSORS"
+}
+let playerWon = GameStatus.NOT_WON;
 
 let optionsArray = [{value: "", nodeText: "-- Player's choice --"},
-    {value: "ROCK", nodeText: "ROCK"},
-    {value: "PAPER", nodeText: "PAPER"},
-    {value: "SCISSORS", nodeText: "SCISSORS"}];
+    {value: Selection.ROCK, nodeText: Selection.ROCK},
+    {value: Selection.PAPER, nodeText: Selection.PAPER},
+    {value: Selection.SCISSORS, nodeText: Selection.SCISSORS}];
 
 const playGame = async (onWin, onLose) => {
     let player = getPlayerProgressData();
     let attempts = player.attempts;
 
-    const options_list = ["ROCK", "PAPER", "SCISSORS"]
+    const options_list = [Selection.ROCK, Selection.PAPER, Selection.SCISSORS]
     const choiceNumber = Math.floor(Math.random() * 3);
 
     computerChoice = options_list[choiceNumber];
     playerChoice = selectEl.value;
 
 
-    if (playerChoice === "ROCK" || playerChoice === "PAPER" || playerChoice === "SCISSORS") {
+    if (playerChoice === Selection.ROCK || playerChoice === Selection.PAPER || playerChoice === Selection.SCISSORS) {
 
         await incrementAttempts()
 
-        if (playerChoice === "ROCK" && computerChoice === "SCISSORS" ||
-            playerChoice === "PAPER" && computerChoice === "ROCK" ||
-            playerChoice === "SCISSORS" && computerChoice === "PAPER") {
+        if (playerChoice === Selection.ROCK && computerChoice === Selection.SCISSORS ||
+            playerChoice === Selection.PAPER && computerChoice === Selection.ROCK ||
+            playerChoice === Selection.SCISSORS && computerChoice === Selection.PAPER) {
 
-            playerWon = "Yes"
+            playerWon = GameStatus.WON
             selectEl.disabled = true;
             buttonEl.disabled = true;
 
             showResultCard("win", `You choose: ${playerChoice}. Computer choose: ${computerChoice}`)
             onWin()
 
-        } else if (computerChoice === "ROCK" && playerChoice === "SCISSORS" ||
-            computerChoice === "PAPER" && playerChoice === "ROCK" ||
-            computerChoice === "SCISSORS" && playerChoice === "PAPER") {
+        } else if (computerChoice === Selection.ROCK && playerChoice === Selection.SCISSORS ||
+            computerChoice === Selection.PAPER && playerChoice === Selection.ROCK ||
+            computerChoice === Selection.SCISSORS && playerChoice === Selection.PAPER) {
 
-            playerWon = "No"
+            playerWon = GameStatus.NOT_WON
 
-            if (attempts < 2) {
+            if (attempts < Attempts.SECOND) {
                 showResultCard("try", `You choose: ${playerChoice}. Computer choose: ${computerChoice}.`)
             }
 
         } else if (computerChoice === playerChoice) {
 
-            playerWon = "Draw"
+            playerWon = GameStatus.DRAW
 
-            if (attempts < 2) {
+            if (attempts < Attempts.SECOND) {
                 showResultCard("draw", `You choose: ${playerChoice}. Computer choose: ${computerChoice}.`)
             }
 
@@ -78,7 +89,7 @@ const playGame = async (onWin, onLose) => {
         warningMessageModal("Please select a valid option.")
     }
 
-    if (attempts === 2 && (playerWon === "No" || playerWon === "Draw")) {
+    if (attempts === Attempts.SECOND && (playerWon === GameStatus.NOT_WON || playerWon === GameStatus.DRAW)) {
         selectEl.disabled = true;
         buttonEl.disabled = true;
         showResultCard("lose", `Your choose: ${playerChoice}. Computer's choose: ${computerChoice}. No attempts left. You lose.`)
